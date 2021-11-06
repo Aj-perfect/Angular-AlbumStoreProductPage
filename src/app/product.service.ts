c07db860431698ee7a9b4c2faad912f6d542ee21
Adekola Ajayi <ajayialex80@gmail.com>
2021-11-06 12:34:59 +0100

    git add .

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,9 @@
 
 import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
+import { Observable } from 'rxjs';
 import {map} from 'rxjs/add/operator/map';
+import { Album } from './album';
 
 
 @Injectable()
@@ -11,8 +13,8 @@ export class ProductService {
 
   private _albumUrl = "../assets/album.json";
 
-  getAlbum (id: number){
-    return this._http.get(this._albumUrl).pipe(map(response => response.json)) 
+  getAlbum (id: number): Observable<Album>{
+    return this._http.get(this._albumUrl).pipe(map(response => <Album>response.json())) 
   }
 
 }
